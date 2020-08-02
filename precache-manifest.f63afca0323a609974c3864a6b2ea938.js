0a89291b3e71d221b791fef649a3096e2c4f59ea
Hossain Khademian <hossain.khademian@gmail.com>
2020-08-02 13:20:53 +0430

    first publish

---
+++ precache-manifest.f63afca0323a609974c3864a6b2ea938.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b816d760d070e571fbf572bc3e06ad67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "544accabfa68dcd7c67f",
+    "url": "/static/css/2.29c4d543.chunk.css"
+  },
+  {
+    "revision": "f169b45a2cda8a443a15",
+    "url": "/static/css/main.77353a58.chunk.css"
+  },
+  {
+    "revision": "544accabfa68dcd7c67f",
+    "url": "/static/js/2.81e924c4.chunk.js"
+  },
+  {
+    "revision": "e7d6ac3533400a58864ae2baea7819f2",
+    "url": "/static/js/2.81e924c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f169b45a2cda8a443a15",
+    "url": "/static/js/main.3428a4f3.chunk.js"
+  },
+  {
+    "revision": "06a5a0d96bd48fe9ed55",
+    "url": "/static/js/runtime-main.5e911c45.js"
+  },
+  {
+    "revision": "410725570d34883411e795867e90b04b",
+    "url": "/static/media/boy.41072557.png"
+  }
+]);
